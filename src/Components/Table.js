967071621dc72f6964a58b179bf81e3171c66e9e
Ronald <ronald@Ronalds-MacBook-Pro.local>
2023-08-14 12:25:44 +0100

    second commit

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,34 +1,25 @@
-import React, { useState } from 'react';
-import "./Table.css";
-
-export default function Table({ charactersdata }) {
-    const [deleter, setDelete] = useState(charactersdata);
-
-    function handleDelete(indexId) {
-        const updatedCharacters = deleter.filter((_,i)=> i !== indexId)
-        setDelete(updatedCharacters);
+import React, { useState } from 'react'
+import "./Table.css"
+export default function Table({charactersdata}) {
+    const [deleter,setdeleter] = useState(charactersdata)
+    function handleremove(index) {
+        const initialremove = deleter.filter((_,i) => i !== index);
+      setdeleter(initialremove)
     }
-
-    return (
-        <div>
-            <h1>The Occupation</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Name</td>
-                        <td>Job</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {deleter.map((data, key) => (
-                        <tr key={key}>
-                            <td>{data.name}</td>
-                            <td>{data.job}</td>
-                            <td><button onClick={() => handleDelete(key)}>Delete</button></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
+  return (
+    <div>
+        <h1>asdfhg</h1>
+        <table>
+            <tbody>
+               {deleter.map((item,key) =>(
+                <tr key={key}>
+                    <td>{item.name}</td>
+                    <td>{item.job}</td>
+                    <td><button onClick={()=>{handleremove(key)}}>Delete</button></td>
+                </tr>
+               ))}
+            </tbody>
+        </table>
+    </div>
+  )
 }
